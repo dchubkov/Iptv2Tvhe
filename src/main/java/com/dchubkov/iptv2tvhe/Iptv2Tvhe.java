bb37192c50f084761d02756305dee30b78816d5c
Dmitry Chubkov <dchubkov@icloud.com>
2015-11-25 12:37:07 +0200

    3rd commit

--- src/main/java/com/dchubkov/iptv2tvhe/Iptv2Tvhe.java
+++ src/main/java/com/dchubkov/iptv2tvhe/Iptv2Tvhe.java
@@ -9,6 +9,6 @@ public class Iptv2Tvhe
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!!" );
     }
 }
